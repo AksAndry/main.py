5f3808be4fc9ab0316b82452cf4b325d38d26cf6
AksAndry <aks.andry04@gmail.com>
2024-05-26 17:56:39 +0500

    Add files via upload

---
+++ main.ру.py
@@ -0,0 +1,18 @@
+a = 'длина слова'
+print(a)
+print(len(a))
+first = 8
+second = 6
+print('summa=',(first + second))
+print('diff=', (first - second))
+first = 8
+second = 6
+third = 10
+print('mean=', (first + second + third)/3)
+first_string = 'Вторник'
+second_string = 'Понедельник'
+print(second_string,',', first_string)
+a = 10
+b = 11
+c = 12
+print(((((a*b) + (a*c)))**3)/2)
